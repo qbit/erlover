39dcc14926b07851cbcefb3356f17d4a3c8d9232
Aaron Bieber <deftly@gmail.com>
2014-04-21 14:02:29 -0600

    initial

---
+++ src/erlover.erl
@@ -0,0 +1,13 @@
+-module(erlover).
+
+-export([send/3, send/1]).
+
+send(M) ->
+    St = [K ++ "=" ++ V || {K, V} <- M],
+    post(string:join(St, "&")).
+
+send(Token, User, Parts) ->
+    Nl = lists:append([{"token", Token}, {"user", User}], Parts),
+    send(Nl).
+
+post(Body) -> httpc:request(post, {"https://api.pushover.net/1/messages.json", [], "application/x-www-form-urlencoded", Body}, [], []).
